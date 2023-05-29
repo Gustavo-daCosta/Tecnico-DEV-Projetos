eb878b8ced0ea645d091a2ccc467823a8e0b4c5f
Gustavo-daCosta <gustavo.costa.senaidev@gmail.com>
2023-05-29 17:23:33 -0300

    Projeto gamer FullStack MVC iniciado

---
+++ Projeto Gamer ASP.NET MVC/Migrations/20230529201143_InitialCreate.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Projeto_Gamer_ASP.NET_MVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Equipe",
+                columns: table => new
+                {
+                    IdEquipe = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Imagem = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Equipe", x => x.IdEquipe);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Jogador",
+                columns: table => new
+                {
+                    IdJogador = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Senha = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IdEquipe = table.Column<int>(type: "int", nullable: false),
+                    EquipeIdEquipe = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jogador", x => x.IdJogador);
+                    table.ForeignKey(
+                        name: "FK_Jogador_Equipe_EquipeIdEquipe",
+                        column: x => x.EquipeIdEquipe,
+                        principalTable: "Equipe",
+                        principalColumn: "IdEquipe");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Jogador_EquipeIdEquipe",
+                table: "Jogador",
+                column: "EquipeIdEquipe");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Jogador");
+
+            migrationBuilder.DropTable(
+                name: "Equipe");
+        }
+    }
+}
---
+++ Projeto Gamer ASP.NET MVC/Models/Equipe.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Projeto_Gamer_ASP.NET_MVC.Models
+{
+    public class Equipe
+    {
+        [Key]
+        public int IdEquipe { get; set; }
+        public string? Nome { get; set; }
+        public string? Imagem { get; set; }
+
+        public ICollection<Jogador>? Jogador { get; set; }
+
+        public static void Cadastrar(Equipe equipe) {}
+
+        // public List<Equipe> Listar() {}
+
+
+    }
+}
