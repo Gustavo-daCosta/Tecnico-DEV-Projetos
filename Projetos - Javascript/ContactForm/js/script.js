ce2906216e2fbd293a244bb3728dba8dd75c5459
Gustavo-daCosta <gustavo.costa70@aluno.senai.br>
2023-10-26 17:13:09 -0300

    Projeto ContactForm criado

---
+++ Projetos - Javascript/ContactForm/js/script.js
@@ -0,0 +1,27 @@
+function cadastrar() {
+    event.preventDefault();
+
+    let nome = document.getElementById('nome').value;
+    let sobrenome = document.getElementById('sobrenome').value;
+    let email = document.getElementById('email').value;
+    let pais = document.getElementById('pais').value;
+    let ddd = document.getElementById('ddd').value;
+    let telefone = document.getElementById('telefone').value; 
+    let cep = document.getElementById('cep').value;
+    let rua = document.getElementById('rua').value;
+    let numero = document.getElementById('numero').value;
+    let complemento = document.getElementById('complemento').value;
+    let bairro = document.getElementById('bairro').value;
+    let cidade = document.getElementById('cidade').value;
+    let uf = document.getElementById('UF').value;
+
+    const objDados = {  }
+}
+
+function chamarApi() {}
+
+function preencherEndereco() {
+
+}
+
+function limparDados() {}
