417783525806e6a27deba4f6088aca80f70db7a5
Gustavo-daCosta <gustavo.costa70@aluno.senai.br>
2023-10-30 16:28:07 -0300

    ContactForm: Finalização da requisição a API

--- Projetos - Javascript/ContactForm/js/script.js
+++ Projetos - Javascript/ContactForm/js/script.js
@@ -1,27 +1,69 @@
-function cadastrar() {
+async function cadastrar(event) {
     event.preventDefault();
-
-    let nome = document.getElementById('nome').value;
-    let sobrenome = document.getElementById('sobrenome').value;
-    let email = document.getElementById('email').value;
-    let pais = document.getElementById('pais').value;
-    let ddd = document.getElementById('ddd').value;
-    let telefone = document.getElementById('telefone').value; 
+    
     let cep = document.getElementById('cep').value;
-    let rua = document.getElementById('rua').value;
-    let numero = document.getElementById('numero').value;
+    let logradouro = document.getElementById('rua').value;
     let complemento = document.getElementById('complemento').value;
     let bairro = document.getElementById('bairro').value;
-    let cidade = document.getElementById('cidade').value;
+    let localidade = document.getElementById('cidade').value;
     let uf = document.getElementById('UF').value;
 
-    const objDados = {  }
+    const urlLocal = "http://127.0.0.1:5500/index.html"
+    const objDados = { cep, logradouro, complemento, bairro, localidade, uf };
+
+    try {
+        const promise = await fetch(urlLocal, {
+            body: JSON.stringify(objDados),
+            headers: {"Content-Type": "application/json"},
+            method: "post",
+        });
+    } catch (error) {
+        alert("Deu ruim, erro: " + error);
+    }
 }
 
-function chamarApi() {}
+async function chamarApi() {
+    const cep = document.getElementById("cep").value;
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
 
-function preencherEndereco() {
+    try {
+        // resolvida ou fullfilled
+        const promise = await fetch(url);
+        const resposta = await promise.json();
 
+        console.log(resposta);
+
+        if ('erro' in resposta) {
+            throw new Error();
+        }
+
+        preencherEndereco(resposta);
+    } catch (error) {
+        // rejected
+        console.log("Deu ruim na API");
+    }
 }
 
-function limparDados() {}
+function preencherEndereco(resposta) {
+    document.getElementById('rua').value = resposta.logradouro;
+    document.getElementById('complemento').value = resposta.complemento;
+    document.getElementById('bairro').value = resposta.bairro;
+    document.getElementById('cidade').value = resposta.localidade;
+    document.getElementById('UF').value = resposta.uf;
+}
+
+function limparDados() {
+    document.getElementById('nome').value = "";
+    document.getElementById('sobrenome').value = "";
+    document.getElementById('email').value = "";
+    document.getElementById('pais').value = "";
+    document.getElementById('ddd').value = "";
+    document.getElementById('telefone').value = "";
+    document.getElementById('cep').value = "";
+    document.getElementById('rua').value = "";
+    document.getElementById('numero').value = "";
+    document.getElementById('complemento').value = "";
+    document.getElementById('bairro').value = "";
+    document.getElementById('cidade').value = "";
+    document.getElementById('UF').value = "";
+}
